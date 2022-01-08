6042b57af788e79f4cdc381ab545760f0fb2afb0
GabrielNBDS <gabriel.nbds@gmail.com>
2022-01-08 16:05:25 -0300

    📦 NEW: Added custom validator messages in CreateRoomValidator

--- app/Validators/Rooms/CreateRoomValidator.ts
+++ app/Validators/Rooms/CreateRoomValidator.ts
@@ -1,12 +1,15 @@
-import { schema } from '@ioc:Adonis/Core/Validator'
+import { rules, schema } from '@ioc:Adonis/Core/Validator'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class CreateRoomValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    title: schema.string({ trim: true }),
+    title: schema.string({ trim: true }, [rules.minLength(4)]),
   })
 
-  public messages = {}
+  public messages = {
+    'title.minLength': 'O "título" deve conter no mínimo 4 caracteres',
+    'title.required': 'O "título" não pode estar em branco',
+  }
 }
