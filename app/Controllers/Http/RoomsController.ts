0345df732698f3a36181614d9388f8e197d4c435
GabrielNBDS <gabriel.nbds@gmail.com>
2022-01-08 15:57:56 -0300

    📦 NEW: Created "store" method in rooms controller

--- app/Controllers/Http/RoomsController.ts
+++ app/Controllers/Http/RoomsController.ts
@@ -1,3 +1,13 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import CreateRoomService from 'App/Services/Rooms/CreateRoomService'
+import CreateRoomValidator from 'App/Validators/Rooms/CreateRoomValidator'
 
-export default class RoomsController {}
+export default class RoomsController {
+  public async store({ request }: HttpContextContract) {
+    const { title } = await request.validate(CreateRoomValidator)
+
+    const room = await CreateRoomService.execute(title)
+
+    return room
+  }
+}
