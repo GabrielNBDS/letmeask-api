cced4eadd496bd00275cd021626b20281ca13d33
GabrielNBDS <gabriel.nbds@gmail.com>
2022-01-08 16:22:59 -0300

    📦 NEW: Added "close" method to RoomsController

--- app/Controllers/Http/RoomsController.ts
+++ app/Controllers/Http/RoomsController.ts
@@ -1,4 +1,5 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import CloseRoomService from 'App/Services/Rooms/CloseRoomService'
 import CreateRoomService from 'App/Services/Rooms/CreateRoomService'
 import CreateRoomValidator from 'App/Validators/Rooms/CreateRoomValidator'
 
@@ -7,6 +8,14 @@ export default class RoomsController {
     const { name } = await request.validate(CreateRoomValidator)
 
     const room = await CreateRoomService.execute(name)
+
+    return room
+  }
+
+  public async close({ params }: HttpContextContract) {
+    const { id } = params
+
+    const room = await CloseRoomService.execute(id)
 
     return room
   }
