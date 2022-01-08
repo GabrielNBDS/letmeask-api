ff70b13772f496e6ef92335aca0edb1e81e9c2f4
GabrielNBDS <gabriel.nbds@gmail.com>
2022-01-08 16:31:24 -0300

    📦 NEW: Returning rooms.isOpen on create

--- app/Services/Rooms/CreateRoomService.ts
+++ app/Services/Rooms/CreateRoomService.ts
@@ -2,7 +2,12 @@ import Room from 'App/Models/Room'
 
 class CreateRoomService {
   public static async execute(name: string) {
-    const room = await Room.create({ name })
+    const room = new Room()
+
+    room.name = name
+    room.isOpen = true
+
+    await room.save()
 
     return room
   }
