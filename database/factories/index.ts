3f049e21297563e3b91d4267795640fb688b7f69
GabrielNBDS <gabriel.nbds@gmail.com>
2022-01-09 15:27:14 -0300

    📦 NEW: Created Question Factory

--- database/factories/index.ts
+++ database/factories/index.ts
@@ -1,9 +1,18 @@
 import Room from 'App/Models/Room'
 import Factory from '@ioc:Adonis/Lucid/Factory'
+import Question from 'App/Models/Question'
+
+export const QuestionFactory = Factory.define(Question, ({ faker }) => {
+  return {
+    content: faker.lorem.words(100),
+  }
+}).build()
 
 export const RoomFactory = Factory.define(Room, ({ faker }) => {
   return {
     name: faker.name.title(),
     isOpen: true,
   }
-}).build()
+})
+  .relation('questions', () => QuestionFactory)
+  .build()
