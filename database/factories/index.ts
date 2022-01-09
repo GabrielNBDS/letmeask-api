a42ca5edf7e1294583335de6a9e3cdf937755bfa
GabrielNBDS <gabriel.nbds@gmail.com>
2022-01-08 21:41:21 -0300

    📦 NEW: Added isOpen to RoomFactory

--- database/factories/index.ts
+++ database/factories/index.ts
@@ -4,5 +4,6 @@ import Factory from '@ioc:Adonis/Lucid/Factory'
 export const RoomFactory = Factory.define(Room, ({ faker }) => {
   return {
     name: faker.name.title(),
+    isOpen: true,
   }
 }).build()
