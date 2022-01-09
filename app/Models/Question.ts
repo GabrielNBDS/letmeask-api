07c297d94a969b1b5ff5dd523504f721e0ec0fd3
GabrielNBDS <gabriel.nbds@gmail.com>
2022-01-08 23:52:19 -0300

    📦 NEW: Created Question model

---
+++ app/Models/Question.ts
@@ -0,0 +1,38 @@
+import { DateTime } from 'luxon'
+import { BaseModel, beforeCreate, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import { string } from '@poppinss/utils/build/helpers'
+import Room from './Room'
+
+export default class Question extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public content: string
+
+  @column()
+  public truncated: string
+
+  @column()
+  public answer: string
+
+  @column()
+  public isAnswered: boolean
+
+  @column()
+  public roomId: number
+
+  @belongsTo(() => Room)
+  public user: BelongsTo<typeof Room>
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @beforeCreate()
+  public static truncateContent(question: Question) {
+    question.truncated = string.truncate(question.content, 177)
+  }
+}
