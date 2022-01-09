54c30958b24993abe3d45b4c4f861202fe151bd5
GabrielNBDS <gabriel.nbds@gmail.com>
2022-01-09 00:56:58 -0300

    📦 NEW: Created CreateQuestion tests

---
+++ app/Services/Questions/CreateQuestionService.spec.ts
@@ -0,0 +1,32 @@
+import Database from '@ioc:Adonis/Lucid/Database'
+import Question from 'App/Models/Question'
+import { RoomFactory } from 'Database/factories'
+import test from 'japa'
+import CreateQuestionService from './CreateQuestionService'
+import faker from 'faker'
+
+test.group('CreateQuestionService', (group) => {
+  group.beforeEach(async () => {
+    await Database.beginGlobalTransaction()
+  })
+
+  group.afterEach(async () => {
+    await Database.rollbackGlobalTransaction()
+  })
+
+  test('Create Question', async (assert) => {
+    const room = await RoomFactory.create()
+
+    const question = await CreateQuestionService.execute(room.slug, faker.lorem.words(100))
+
+    assert.instanceOf(question, Question)
+  })
+
+  test('Truncate content', async (assert) => {
+    const room = await RoomFactory.create()
+
+    const question = await CreateQuestionService.execute(room.slug, faker.lorem.words(100))
+
+    assert.equal(question.truncated.length, 180)
+  })
+})
