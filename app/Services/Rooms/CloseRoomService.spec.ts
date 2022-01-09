d5ae28b58a794e67e83a075a1c1af2f327a7ac05
GabrielNBDS <gabriel.nbds@gmail.com>
2022-01-08 21:22:00 -0300

    📦 NEW: Created CloseRoom test

---
+++ app/Services/Rooms/CloseRoomService.spec.ts
@@ -0,0 +1,22 @@
+import Database from '@ioc:Adonis/Lucid/Database'
+import { RoomFactory } from 'Database/factories'
+import test from 'japa'
+import CloseRoomService from './CloseRoomService'
+
+test.group('CloseRoomService', (group) => {
+  group.beforeEach(async () => {
+    await Database.beginGlobalTransaction()
+  })
+
+  group.afterEach(async () => {
+    await Database.rollbackGlobalTransaction()
+  })
+
+  test('Close room', async (assert) => {
+    const room = await RoomFactory.create()
+
+    const closedRoom = await CloseRoomService.execute(room.id)
+
+    assert.isFalse(closedRoom.isOpen)
+  })
+})
