a5ec72f19661614d31ea8a1e7456c7f63de7fd25
GabrielNBDS <gabriel.nbds@gmail.com>
2022-01-08 23:51:59 -0300

    📦 NEW: Created Question migration

---
+++ database/migrations/1641689414499_questions.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Questions extends BaseSchema {
+  protected tableName = 'questions'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('truncated', 180)
+      table.text('content').notNullable()
+      table.text('answer')
+      table.boolean('is_answered').defaultTo(false)
+      table.integer('room_id').unsigned().references('rooms.id').onDelete('CASCADE')
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
