88a3348003197df246a7a156ef3d896e737f106d
GabrielNBDS <gabriel.nbds@gmail.com>
2022-01-12 11:50:20 -0300

    📦 NEW: Created IndexQuestionsService

---
+++ app/Services/Questions/IndexQuestionsService.ts
@@ -0,0 +1,24 @@
+import Question from 'App/Models/Question'
+import Room from 'App/Models/Room'
+
+class IndexQuestionsService {
+  public static async execute(slug: string, loadAnswered: boolean = false, page: number = 1) {
+    const room = await Room.findByOrFail('slug', slug)
+
+    const questionsQuery = Question.query().where('room_id', '=', room.id)
+
+    questionsQuery.select('id', 'truncated')
+
+    if (loadAnswered) {
+      questionsQuery.where('is_answered', true)
+    } else {
+      questionsQuery.where('is_answered', false)
+    }
+
+    const questions = await questionsQuery.paginate(page, 10)
+
+    return questions.serialize()
+  }
+}
+
+export default IndexQuestionsService
