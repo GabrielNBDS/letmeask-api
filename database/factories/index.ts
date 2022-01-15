732d3529cffcd01b3adc26e1fdfbf67047f66d19
GabrielNBDS <gabriel.nbds@gmail.com>
2022-01-15 01:24:18 -0400

    📦 NEW: Created user factory

--- database/factories/index.ts
+++ database/factories/index.ts
@@ -1,6 +1,17 @@
 import Room from 'App/Models/Room'
 import Factory from '@ioc:Adonis/Lucid/Factory'
 import Question from 'App/Models/Question'
+import User from 'App/Models/User'
+
+export const UserFactory = Factory.define(User, ({ faker }) => {
+  const name = faker.name.firstName()
+  return {
+    name,
+    login: faker.internet.userName(name),
+    email: faker.internet.email(),
+    avatar: faker.internet.avatar(),
+  }
+}).build()
 
 export const QuestionFactory = Factory.define(Question, ({ faker }) => {
   return {
